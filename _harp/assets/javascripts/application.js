b063097e4e604fae6e82aedd6e0e838fe7820a90
credesomniis <sofie.luyckx@hotmail.com>
2016-12-05 23:48:30 +0100

    Upload site

---
+++ _harp/assets/javascripts/application.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+sr.reveal('.one');
+sr.reveal('.two');
+sr.reveal('.tree');
+sr.reveal('.four');
+sr.reveal('.five');
+sr.reveal('.six');
+sr.reveal('.seven');
+sr.reveal('.eight');
+sr.reveal('.nine');
+sr.reveal('.ten');
+sr.reveal('.eleven');
+sr.reveal('.twelve');
+sr.reveal('.thriteen');
+sr.reveal('.fourteen');
+sr.reveal('.fifteen');
+sr.reveal('.sixteen');
