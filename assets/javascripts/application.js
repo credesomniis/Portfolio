e8f5a6895a09f5b55af66c7bc826dac2427d328c
credesomniis <sofie.luyckx@hotmail.com>
2016-12-06 18:13:03 +0100

    Update site

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,17 +1,24 @@
-window.sr = ScrollReveal();
-sr.reveal('.one');
-sr.reveal('.two');
-sr.reveal('.tree');
-sr.reveal('.four');
-sr.reveal('.five');
-sr.reveal('.six');
-sr.reveal('.seven');
-sr.reveal('.eight');
-sr.reveal('.nine');
-sr.reveal('.ten');
-sr.reveal('.eleven');
-sr.reveal('.twelve');
-sr.reveal('.thriteen');
-sr.reveal('.fourteen');
-sr.reveal('.fifteen');
-sr.reveal('.sixteen');
+function main() {
+
+  window.sr = ScrollReveal();
+  sr.reveal('.one');
+  sr.reveal('.two');
+  sr.reveal('.tree');
+  sr.reveal('.four');
+  sr.reveal('.five');
+  sr.reveal('.six');
+  sr.reveal('.seven');
+  sr.reveal('.eight');
+  sr.reveal('.nine');
+  sr.reveal('.ten');
+  sr.reveal('.eleven');
+  sr.reveal('.twelve');
+  sr.reveal('.thriteen');
+  sr.reveal('.fourteen');
+  sr.reveal('.fifteen');
+  sr.reveal('.sixteen');
+}
+
+window.onload = function() {
+       main();
+}
